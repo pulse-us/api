26ad9449524b9b0e268e9db64058986a82bfb579
brianlindsy <brianl9864@gmail.com>
2016-05-09 12:28:27 -0400

    PULSE-42 tests and doc

--- pulse/broker/src/test/java/gov/ca/emsa/pulse/broker/BrokerApplicationTests.java
+++ pulse/broker/src/test/java/gov/ca/emsa/pulse/broker/BrokerApplicationTests.java
@@ -1,14 +1,26 @@
 package gov.ca.emsa.pulse.broker;
 
+import static org.junit.Assert.*;
 import gov.ca.emsa.pulse.broker.BrokerApplication;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = BrokerApplication.class)
-public class BrokerApplicationTests {
+@WebAppConfiguration
+public class BrokerApplicationTests extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void getDirectoriesTest(){
+		assertEquals(super.countRowsInTable("organization"), 3);
 	}
 
 }
