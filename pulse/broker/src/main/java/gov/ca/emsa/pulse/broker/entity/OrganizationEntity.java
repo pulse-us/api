3bd908b48cd3342c52f9d4167897d2a3a6acaf7c
Katy Ekey <kekey@ainq.com>
2016-05-18 09:46:45 -0400

    development:

--- pulse/broker/src/main/java/gov/ca/emsa/pulse/broker/entity/OrganizationEntity.java
+++ pulse/broker/src/main/java/gov/ca/emsa/pulse/broker/entity/OrganizationEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="organization", schema="pulse")
+@Table(name="organization")
 public class OrganizationEntity {
 	
 	@Id
