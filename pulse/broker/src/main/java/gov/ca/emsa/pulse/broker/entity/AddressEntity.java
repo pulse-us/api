25450ae300c9b4b77b55e3ef70b870fa4fa3f2b8
Katy Ekey <kekey@ainq.com>
2016-05-18 10:03:58 -0400

    PULSE-51: clean up merge conflicts

--- pulse/broker/src/main/java/gov/ca/emsa/pulse/broker/entity/AddressEntity.java
+++ pulse/broker/src/main/java/gov/ca/emsa/pulse/broker/entity/AddressEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="address")
+@Table(name="address", schema="pulse")
 public class AddressEntity {
 	
 	@Id 
--- pulse/broker/src/main/java/gov/ca/emsa/pulse/broker/entity/PatientEntity.java
+++ pulse/broker/src/main/java/gov/ca/emsa/pulse/broker/entity/PatientEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="patient")
+@Table(name="patient", schema="pulse")
 public class PatientEntity {
 	
 	@Id 
