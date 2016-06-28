f6723e6c3e7706dfbc8961d305e25d7e386b84d3
Brian Lindsey <brianl9864@gmail.com>
2016-06-28 12:54:08 -0400

    change rest endpoints to agreed upon formats

--- pulse/service/src/main/java/gov/ca/emsa/pulse/service/User.java
+++ pulse/service/src/main/java/gov/ca/emsa/pulse/service/User.java
@@ -3,6 +3,8 @@ package gov.ca.emsa.pulse.service;
 public class User {
 	
 	private String name;
+	private String userToken;
+	private String acf;
 
 	public String getName() {
 		return name;
@@ -11,5 +13,23 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getUserToken() {
+		return userToken;
+	}
+
+	public void setUserToken(String userToken) {
+		this.userToken = userToken;
+	}
+
+	public String getAcf() {
+		return acf;
+	}
+
+	public void setAcf(String acf) {
+		this.acf = acf;
+	}
+	
+	
 
 }
