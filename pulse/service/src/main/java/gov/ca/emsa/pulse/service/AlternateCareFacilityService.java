b2fd1f40fb362836c5900f5ffe21a60f58b3a7f6
Brian Lindsey <brianl9864@gmail.com>
2016-06-29 10:06:34 -0400

    fix requestmapping annotation

--- pulse/service/src/main/java/gov/ca/emsa/pulse/service/AlternateCareFacilityService.java
+++ pulse/service/src/main/java/gov/ca/emsa/pulse/service/AlternateCareFacilityService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.github.tomakehurst.wiremock.http.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class AlternateCareFacilityService {
