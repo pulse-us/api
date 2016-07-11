1cb06efae6f5610b2c54de08e110657c316ecb29
Brian Lindsey <brianl9864@gmail.com>
2016-07-11 09:27:00 -0400

    add common objects to broker/common project

---
+++ pulse/common/src/main/java/gov/ca/emsa/pulse/common/domain/CreatePatientRequest.java
@@ -0,0 +1,20 @@
+package gov.ca.emsa.pulse.common.domain;
+
+import java.util.List;
+
+public class CreatePatientRequest {
+	private List<Long> patientRecordIds;
+	private Patient patient;
+	public List<Long> getPatientRecordIds() {
+		return patientRecordIds;
+	}
+	public void setPatientRecordIds(List<Long> patientRecordIds) {
+		this.patientRecordIds = patientRecordIds;
+	}
+	public Patient getPatient() {
+		return patient;
+	}
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+}
---
+++ pulse/common/src/main/java/gov/ca/emsa/pulse/common/domain/Document.java
@@ -0,0 +1,28 @@
+package gov.ca.emsa.pulse.common.domain;
+
+public class Document {
+	private String id;
+	private String name;
+	private Long orgMapId;
+	
+	public Document() {}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getOrgMapId() {
+		return orgMapId;
+	}
+	public void setOrgMapId(Long orgMapId) {
+		this.orgMapId = orgMapId;
+	}
+}
