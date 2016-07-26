befa961254cf61628cb15b51f6efd4889add0048
Brian Lindsey <brianl9864@gmail.com>
2016-07-26 10:19:38 -0400

    add DocumentWrapper to match expected UI response type

---
+++ pulse/common/src/main/java/gov/ca/emsa/pulse/common/domain/DocumentWrapper.java
@@ -0,0 +1,19 @@
+package gov.ca.emsa.pulse.common.domain;
+
+public class DocumentWrapper {
+	
+	private String data;
+	
+	public DocumentWrapper(String data){
+		this.data = data;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
