13e3c00d17bca44a1c5abda65b75070fbfe90ae0
Brian Lindsey <brianl9864@gmail.com>
2016-08-09 07:40:41 -0400

    add swagger api

--- pulse/service/src/main/java/gov/ca/emsa/pulse/config/PULSEAuthenticationSecurityConfig.java
+++ pulse/service/src/main/java/gov/ca/emsa/pulse/config/PULSEAuthenticationSecurityConfig.java
@@ -10,21 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsChecker;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebSecurity
 @Configuration
@@ -40,6 +32,7 @@ public class PULSEAuthenticationSecurityConfig extends WebSecurityConfigurerAdap
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/health**");
+		web.ignoring().antMatchers("/api-docs**");
 	}
 	
 	@Override
