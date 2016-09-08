c09757f06fbffe0ef0abc61e4f25756a4a5becfe
Brian Lindsey <brianl9864@gmail.com>
2016-09-08 11:03:12 -0400

    add consumer service

--- pulse/service/src/main/java/gov/ca/emsa/pulse/service/EHealthQueryConsumerService.java
+++ pulse/service/src/main/java/gov/ca/emsa/pulse/service/EHealthQueryConsumerService.java
@@ -1,117 +1,16 @@
 package gov.ca.emsa.pulse.service;
 
-import java.util.ArrayList;
+import javax.xml.soap.SOAPException;
 
-import gov.ca.emsa.pulse.auth.user.JWTAuthenticatedUser;
-import gov.ca.emsa.pulse.common.domain.PatientSearch;
-import gov.ca.emsa.pulse.common.domain.Query;
-import gov.ca.emsa.pulse.xcpd.XcpdUtils;
-import gov.ca.emsa.pulse.xcpd.aqr.Slot;
-import gov.ca.emsa.pulse.xcpd.prpa.cap.qbp.ParameterList;
-import gov.ca.emsa.pulse.xcpd.rds.RetrieveDocumentSetRequest;
-import gov.ca.emsa.pulse.xcpd.soap.DiscoveryRequestSoapEnvelope;
-import gov.ca.emsa.pulse.xcpd.soap.DiscoveryResponseSoapEnvelope;
-import gov.ca.emsa.pulse.xcpd.soap.QueryRequestSoapEnvelope;
-import gov.ca.emsa.pulse.xcpd.soap.QueryResponseSoapEnvelope;
-import gov.ca.emsa.pulse.xcpd.soap.RetrieveDocumentSetRequestSoapEnvelope;
-import gov.ca.emsa.pulse.xcpd.soap.RetrieveDocumentSetResponseSoapEnvelope;
+import ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType;
+import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.MediaType;
+import org.hl7.v3.PRPAIN201305UV02;
+import org.opensaml.common.SAMLException;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-@RestController
-public class EHealthQueryConsumerService {
-	
-	private static final Logger logger = LogManager.getLogger(EHealthQueryConsumerService.class);
-	@Value("${brokerUrl}")
-	private String brokerUrl;
-	
-	@RequestMapping(value = "/patientDiscovery", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
-	public DiscoveryResponseSoapEnvelope patientDiscovery(@RequestBody DiscoveryRequestSoapEnvelope dr) throws JsonProcessingException{
-		PatientSearch patientSearchTerms = new PatientSearch();
-		ParameterList parameterList = dr.sBody.PRPA_IN201305UV02.controlActProcess.getQueryByParameter().parameterList;
-		patientSearchTerms.setDob(parameterList.getLivingSubjectBirthTime().value.value);
-		patientSearchTerms.setGender(parameterList.getLivingSubjectAdministrativeGender().value.code);
-		patientSearchTerms.setGivenName(parameterList.getLivingSubjectName().getValue().given);
-		patientSearchTerms.setFamilyName(parameterList.getLivingSubjectName().getValue().family);
-		
-		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-		ObjectMapper mapper = new ObjectMapper();
-		
-		JWTAuthenticatedUser jwtUser = (JWTAuthenticatedUser) SecurityContextHolder.getContext().getAuthentication();
-		if(jwtUser == null){
-			logger.error("Could not find a logged in user. ");
-		}else{
-			headers.add("User", mapper.writeValueAsString(jwtUser));
-			HttpEntity<PatientSearch> request = new HttpEntity<PatientSearch>(patientSearchTerms, headers);
-			RestTemplate query = new RestTemplate();
-			query.postForObject(brokerUrl + "/search", request, Query.class);
-			logger.info("Request sent to broker from services REST.");
-		}
-		
-		return XcpdUtils.generateDiscoveryResponse(patientSearchTerms.getGivenName(), patientSearchTerms.getFamilyName());
-		
-	}
-	
-	@RequestMapping(value = "/queryRequest", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
-	public QueryResponseSoapEnvelope queryRequest(@RequestBody QueryRequestSoapEnvelope qr) throws JsonProcessingException{
-		//patient id
-		String patientId = qr.body.adhocQueryRequest.adhocQuery.id;
-		//parameters for the documents to return
-		ArrayList<Slot> slots = qr.body.adhocQueryRequest.adhocQuery.slots;
-		
-		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-		ObjectMapper mapper = new ObjectMapper();
-		
-		JWTAuthenticatedUser jwtUser = (JWTAuthenticatedUser) SecurityContextHolder.getContext().getAuthentication();
-		if(jwtUser == null){
-			logger.error("Could not find a logged in user. ");
-		}else{
-			headers.add("User", mapper.writeValueAsString(jwtUser));
-			HttpEntity<PatientSearch> request = new HttpEntity<PatientSearch>(headers);
-			RestTemplate query = new RestTemplate();
-			//query.postForObject(brokerUrl + "/search", request, Query.class);
-			logger.info("Request sent to broker from services REST.");
-		}
-		System.out.println(XcpdUtils.generateQueryResponse());
-		return XcpdUtils.generateQueryResponse();
-	}
-	
-	@RequestMapping(value = "/documentRequest", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
-	public RetrieveDocumentSetResponseSoapEnvelope documentRequest(@RequestBody RetrieveDocumentSetRequestSoapEnvelope rds) throws JsonProcessingException{
-		//patient id
-		RetrieveDocumentSetRequest repositoryId = rds.body.documentSetRequest;
-		
-		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-		ObjectMapper mapper = new ObjectMapper();
-		
-		JWTAuthenticatedUser jwtUser = (JWTAuthenticatedUser) SecurityContextHolder.getContext().getAuthentication();
-		if(jwtUser == null){
-			logger.error("Could not find a logged in user. ");
-		}else{
-			headers.add("User", mapper.writeValueAsString(jwtUser));
-			HttpEntity<PatientSearch> request = new HttpEntity<PatientSearch>(headers);
-			RestTemplate query = new RestTemplate();
-			//query.postForObject(brokerUrl + "/search", request, Query.class);
-			logger.info("Request sent to broker from services REST.");
-		}
-		
-		System.out.println(XcpdUtils.generateDocumentResponse());
-		return XcpdUtils.generateDocumentResponse();
-	}
-
+public interface EHealthQueryConsumerService {
+	public PRPAIN201305UV02 unMarshallPatientDiscoveryRequestObject(String xml) throws SOAPException, SAMLException;
+	public AdhocQueryRequest unMarshallDocumentQueryRequestObject(String xml) throws SAMLException;
+	public RetrieveDocumentSetRequestType unMarshallDocumentSetRetrieveRequestObject(String xml) throws SAMLException;
+	public String createSOAPFault();
 }
