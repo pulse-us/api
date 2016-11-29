2b76e332885ba3fd02c9c9c0791322fef2e0e4a5
Katy Ekey <kekey@ainq.com>
2016-11-29 15:47:37 -0500

    PULSE-284: refine error messages and allow ACF changes to non-name fields

--- pulse/broker/src/main/java/gov/ca/emsa/pulse/service/ApiExceptionControllerAdvice.java
+++ pulse/broker/src/main/java/gov/ca/emsa/pulse/service/ApiExceptionControllerAdvice.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,7 +17,11 @@ public class ApiExceptionControllerAdvice {
 	@ExceptionHandler(AcfChangesNotAllowedException.class)
 	public ResponseEntity<ErrorJSONObject> exception(AcfChangesNotAllowedException e) {
 		e.printStackTrace();
-		return new ResponseEntity<ErrorJSONObject>(new ErrorJSONObject("ACF changes are not allowed."), HttpStatus.BAD_REQUEST);
+		String msg = e.getMessage();
+		if(StringUtils.isEmpty(msg)) {
+			msg = "ACF changes are not allowed.";
+		}
+		return new ResponseEntity<ErrorJSONObject>(new ErrorJSONObject(msg), HttpStatus.BAD_REQUEST);
 	}
 	
 	@ExceptionHandler(InvalidArgumentsException.class)
