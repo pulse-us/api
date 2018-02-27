1df487e166c1ab5b50cdce49ba7962c98287c4ad
Andrew Larned <andrew.larned@gmail.com>
2018-02-27 11:52:09 -0500

    PUL-37: Updated JWT to meet PULSE-US needs

--- pulse/auth/src/main/java/gov/ca/emsa/pulse/auth/authentication/JWTUserConverterImpl.java
+++ pulse/auth/src/main/java/gov/ca/emsa/pulse/auth/authentication/JWTUserConverterImpl.java
@@ -28,94 +28,86 @@ import com.google.common.io.Resources;
 @Service
 public class JWTUserConverterImpl implements JWTUserConverter {
 
-	@Autowired
-	private JWTConsumer jwtConsumer;
+    @Autowired
+    private JWTConsumer jwtConsumer;
 
-	private static final Logger LOG = LoggerFactory.getLogger(JWTUserConverterImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(JWTUserConverterImpl.class);
 
-	public JWTUserConverterImpl(){}
+    public JWTUserConverterImpl(){}
 
-	public User getAuthenticatedUser(String jwt) throws JWTValidationException {
+    public User getAuthenticatedUser(String jwt) throws JWTValidationException {
 
-		JWTAuthenticatedUser user = new JWTAuthenticatedUser();
-		user.setAuthenticated(true);
+        JWTAuthenticatedUser user = new JWTAuthenticatedUser();
+        user.setAuthenticated(true);
 
-		Map<String, Object> validatedClaims = jwtConsumer.consume(jwt);
+        Map<String, Object> validatedClaims = jwtConsumer.consume(jwt);
 
-		if (validatedClaims == null){
-			throw new JWTValidationException("Invalid authentication token.");
-		} else {
-			/*
-			 * Handle the standard claim types. These won't be lists of Strings,
-			 * which we'll be expecting from the claims we are creating ourselves
-			 */
-			Object issuer = validatedClaims.remove("iss");
-			Object audience = validatedClaims.remove("aud");
-			Object issuedAt = validatedClaims.remove("iat");
-			Object notBefore = validatedClaims.remove("nbf");
-			Object expires = validatedClaims.remove("exp");
-			Object jti = validatedClaims.remove("jti");
-            //			Object typ = validatedClaims.remove("typ");
+        if (validatedClaims == null){
+            throw new JWTValidationException("Invalid authentication token.");
+        } else {
+            /*
+             * Handle the standard claim types. These won't be lists of Strings,
+             * which we'll be expecting from the claims we are creating ourselves
+             */
+            Object issuer = validatedClaims.remove("iss");
+            Object audience = validatedClaims.remove("aud");
+            Object issuedAt = validatedClaims.remove("iat");
+            Object notBefore = validatedClaims.remove("nbf");
+            Object expires = validatedClaims.remove("exp");
+            Object jti = validatedClaims.remove("jti");
+            //Object typ = validatedClaims.remove("typ");
 
-			String subject = (String) validatedClaims.remove("sub");
+            String subject = (String) validatedClaims.remove("sub");
 
             LOG.info( "jwt claims" );
             LOG.info( issuer.toString() );
             LOG.info( audience.toString() );
-            LOG.info( issuedAt.toString() );
-            LOG.info( notBefore.toString() );
+            //LOG.info( issuedAt.toString() );
+            //LOG.info( notBefore.toString() );
             LOG.info( expires.toString() );
-            LOG.info( jti.toString() );
+            //LOG.info( jti.toString() );
             //            LOG.info( typ.toString());
 
-			user.setSubjectName(subject);
+            user.setSubjectName(subject);
 
-			List<String> authorities = (List<String>) validatedClaims.get("Authorities");
-			List<String> identityInfo = (List<String>) validatedClaims.get("Identity");
+            List<String> authorities = (List<String>) validatedClaims.get("Authorities");
+            List<String> identityInfo = (List<String>) validatedClaims.get("Identity");
 
-			for (String claim: authorities){
-                //				GrantedPermission permission = new GrantedPermission(claim);
-				user.addPermission(claim);
-			}
+            for (String claim: authorities){
+                //GrantedPermission permission = new GrantedPermission(claim);
+                user.addPermission(claim);
+            }
 
             String user_id = identityInfo.get(0);
             String username = identityInfo.get(1);
-            String auth_source = identityInfo.get(2);
-            String full_name = identityInfo.get(3);
-            String organization = identityInfo.get(4);
-            String purpose_for_use = identityInfo.get(5);
-            String role = identityInfo.get(6);
+            String full_name = identityInfo.get(2);
             String pulseUserId = null;
-            if(identityInfo.size() > 7){
-            	pulseUserId = identityInfo.get(7);
+            if(identityInfo.size() > 3) {
+                pulseUserId = identityInfo.get(3);
             }
 
             user.setuser_id(user_id);
             user.setusername(username);
-            user.setauth_source(auth_source);
             user.setfull_name(full_name);
-            user.setorganization(organization);
-            user.setpurpose_for_use(purpose_for_use);
-            user.setrole(role);
-            if(identityInfo.size() > 7){
-            	user.setPulseUserId(pulseUserId);
-			}
+            if(identityInfo.size() > 3) {
+                user.setPulseUserId(pulseUserId);
+            }
 
-            if (identityInfo.size() > 8) {
-                String acfObjStr = identityInfo.get(8);
-            	try {
-	            	ObjectReader reader = new ObjectMapper().reader().forType(AlternateCareFacility.class);
-	            	AlternateCareFacility acf = reader.readValue(acfObjStr);
-	                user.setAcf(acf);
-            	} catch(JsonProcessingException ex) {
-            		LOG.error("Could not read '" + acfObjStr + "' as AlternateCareFacility", ex);
-            	} catch(IOException io) {
-            		LOG.error("Could not read '" + acfObjStr + "' as AlternateCareFacility", io);
-            	}
+            if (identityInfo.size() > 4) {
+                String acfObjStr = identityInfo.get(4);
+                try {
+                    ObjectReader reader = new ObjectMapper().reader().forType(AlternateCareFacility.class);
+                    AlternateCareFacility acf = reader.readValue(acfObjStr);
+                    user.setAcf(acf);
+                } catch(JsonProcessingException ex) {
+                    LOG.error("Could not read '" + acfObjStr + "' as AlternateCareFacility", ex);
+                } catch(IOException io) {
+                    LOG.error("Could not read '" + acfObjStr + "' as AlternateCareFacility", io);
+                }
             }
 
             LOG.info(user.toString());
-		}
-		return user;
-	}
+        }
+        return user;
+    }
 }
--- pulse/auth/src/main/java/gov/ca/emsa/pulse/auth/user/User.java
+++ pulse/auth/src/main/java/gov/ca/emsa/pulse/auth/user/User.java
@@ -12,58 +12,49 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface User extends UserDetails , Authentication {
 
-	public Long getId();
-	public String getSubjectName();
-	public void setSubjectName(String subject);
+    public Long getId();
+    public String getSubjectName();
+    public void setSubjectName(String subject);
 
-	public void setFirstName(String firstName);
-	public String getFirstName();
-	public void setLastName(String lastName);
-	public String getLastName();
+    public void setFirstName(String firstName);
+    public String getFirstName();
+    public void setLastName(String lastName);
+    public String getLastName();
     public void setEmail(String email);
     public String getEmail();
 
     public String getuser_id();
     public void setuser_id(String user_id);
     public void setusername(String username);
-    public String getauth_source ();
-    public void setauth_source(String auth_source);
     public String getfull_name ();
     public void setfull_name(String full_name);
-    public String getorganization ();
-    public void setorganization(String organization);
-    public String getpurpose_for_use ();
-    public void setpurpose_for_use(String purpose_for_use);
     public String getPulseUserId();
     public void setPulseUserId(String purpose_for_use);
-    public String getrole ();
-    public void setrole(String role);
 
     public void setAcf(AlternateCareFacility acf);
     public AlternateCareFacility getAcf();
 
-	public Set<GrantedPermission> getPermissions();
-	public void addPermission(GrantedPermission permission);
-	public void removePermission(String permissionValue);
+    public Set<GrantedPermission> getPermissions();
+    public void addPermission(GrantedPermission permission);
+    public void removePermission(String permissionValue);
 
+    // UserDetails interface
+    @Override
+    public String getUsername();
 
-	// UserDetails interface
-	@Override
-	public String getUsername();
+    // Authentication Interface
+    @Override
+    public Collection<GrantedPermission> getAuthorities();
 
-	// Authentication Interface
-	@Override
-	public Collection<GrantedPermission> getAuthorities();
+    @Override
+    public Object getPrincipal();
 
-	@Override
-	public Object getPrincipal();
+    @Override
+    public boolean isAuthenticated();
 
-	@Override
-	public boolean isAuthenticated();
+    @Override
+    public void setAuthenticated(boolean arg0) throws IllegalArgumentException;
 
-	@Override
-	public void setAuthenticated(boolean arg0) throws IllegalArgumentException;
-
-	@Override
-	public String getName();
+    @Override
+    public String getName();
 }
